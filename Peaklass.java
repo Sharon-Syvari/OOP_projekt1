f2ec10dd1c7c74185c3c09aa76058d739ef83056
Sharon Süvari <92800043+Sharon-Syvari@users.noreply.github.com>
2022-04-06 21:29:30 +0300

    Update Peaklass.java

--- Peaklass.java
+++ Peaklass.java
@@ -7,26 +7,34 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Peaklass {
-    // Siin vist luua erinevad vastased
-    //Võistleja võistleja1
-    //Võistleja võistleja2
-    //Võistleja võistleja3
-
-    Võistleja[] võistlejad = new Võistleja[2];
-
-    //rünnakud ja kaitsed võiks vist loosida. 2 nõrka ja 1 tugev
-    Kaitsed kaitse1 = new Kaitsed("kaitse1", 20);
-    Kaitsed kaitse2 = new Kaitsed("kaitse2", 10);
-    Kaitsed kaitse3 = new Kaitsed("kaitse3", 10);
-
-    Rünnakud rünnak1 = new Rünnakud("rünnak1", 20);
-    Rünnakud rünnak2 = new Rünnakud("rünnak2", 10);
-    Rünnakud rünnak3 = new Rünnakud("rünnak3", 10);
-
-    Kaitsed[] kõik_kaitsed = new Kaitsed[3];
-    //kõik_kaitsed[0] = kaitse1;
 
     public static void main(String[] args) {
+        // Siin vist luua erinevad vastased
+        Võistleja võistleja1 = new Võistleja("Peeter", 15);
+        Võistleja võistleja2 = new Võistleja("Minni", 25);
+        Võistleja võistleja3 = new Võistleja("Bobiina", 35);
+
+        Võistleja[] vastased = new Võistleja[];
+        vastased[0] = võistleja1;
+        vastased[1] = võistleja2;
+        vastased[2] = võistleja3;
+
+        Võistleja[] võistlejad = new Võistleja[2];
+
+        //rünnakud ja kaitsed võiks vist loosida. 2 nõrka ja 1 tugev
+        Kaitsed kaitse1 = new Kaitsed("kaitse1", 20);
+        Kaitsed kaitse2 = new Kaitsed("kaitse2", 10);
+        Kaitsed kaitse3 = new Kaitsed("kaitse3", 10);
+
+        Rünnakud rünnak1 = new Rünnakud("rünnak1", 20);
+        Rünnakud rünnak2 = new Rünnakud("rünnak2", 10);
+        Rünnakud rünnak3 = new Rünnakud("rünnak3", 10);
+
+        Kaitsed[] kõik_kaitsed = new Kaitsed[3];
+        kõik_kaitsed[0] = kaitse1;
+        kõik_kaitsed[1] = kaitse2;
+        kõik_kaitsed[2] = kaitse3;
+
         //Mingi jutt alustuseks
 
         //Esitletakse reeglid:
@@ -50,13 +58,24 @@ public class Peaklass {
         Scanner scan = new Scanner(System.in);
         System.out.println("Kirjutage oma nimi: ");
         String kasutajanimi = scan.nextLine();
-        System.out.println(kasutajanimi);
 
-        //  Kaklus.võistlejad[0] = Võistleja(...)
+        Võistleja kasutaja = new Võistleja(kasutajanimi, 25);
+        võistlejad[0] = kasutaja;
         //Esitletakse kasutaja omadused
+        
 
         //Lastakse valida vastane(int)
         //  Kaklus.võistlejad[1] = Võistleja(...)
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Valige enda rühmakaaslane: ");
+        System.out.print("1. ");
+        võistleja1.toString();
+        System.out.print("2. ");
+        võistleja2.toString();
+        System.out.print("3. ");
+        int rühmakaasklase_nr = scan.nextInt();
+
+        võistlejad[1] = vastased[rühmakaasklase_nr-1];
 
         //Mäng algab.
         //Loositakse alustaja ja antakse tulemusest ekraanil teada
