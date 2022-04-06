56bb22b59bea353ad9bcc4d295144a090c966bd4
Sharon Süvari <92800043+Sharon-Syvari@users.noreply.github.com>
2022-04-06 15:19:33 +0300

    Update Kaitsed.java

--- Kaitsed.java
+++ Kaitsed.java
@@ -1,1 +1,12 @@
+package src.Rühmatöö_1;
+
+public class Kaitsed {
+    private String nimetus;
+    private int tugevus;
+
+    public Kaitsed(String nimetus, int tugevus) {
+        this.nimetus = nimetus;
+        this.tugevus = tugevus;
+    }
+}
 
