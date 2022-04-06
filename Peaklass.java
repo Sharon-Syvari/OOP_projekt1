2dcc5ded209bda19bb19734e2d9423ce2f6d2ad4
Sharon Süvari <92800043+Sharon-Syvari@users.noreply.github.com>
2022-04-06 14:16:39 +0300

    Update and rename Peaklass to Peaklass.java

---
+++ Peaklass.java
@@ -0,0 +1,24 @@
+package src
+
+public class Peaklass {
+
+    public static void main(String[] args) {
+        //Mingi jutt alustuseks
+
+        //Esitletakse reeglid
+
+        //Küsitakse kasutaja nime
+        //Esitletakse kasutaja omadused
+
+        //Lastakse valida vastane(int)
+
+        //Mäng algab.
+        //Loositakse alustaja ja antakse tulemusest ekraanil teada
+
+        //Igal korral lastakse valida kasutajal rünnaku - või kaitsevõte
+        // ja selle põhjal loositakse tema punktid
+
+        //Kaotab 1 elu see, kellel vähem punkte
+    }
+}
+
