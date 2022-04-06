b6b6e1ef06b134f9bcf652b3b9a87193deff619b
Sharon Süvari <92800043+Sharon-Syvari@users.noreply.github.com>
2022-04-06 15:20:58 +0300

    Update Rünnakud.java

--- Rünnakud.java
+++ Rünnakud.java
@@ -1,1 +1,12 @@
+package src.Rühmatöö_1;
+
+public class Rünnakud {
+    private String nimetus;
+    private int tugevus;
+
+    public Rünnakud(String nimetus, int tugevus) {
+        this.nimetus = nimetus;
+        this.tugevus = tugevus;
+    }
+}
 
