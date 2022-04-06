f0649aeadd178d006b132d024101149392418a65
Sharon Süvari <92800043+Sharon-Syvari@users.noreply.github.com>
2022-04-06 14:15:46 +0300

    Update Kaklus.java

--- Kaklus.java
+++ Kaklus.java
@@ -1,5 +1,29 @@
 package src;
 
 public class Kaklus {
-    
+    // Võistleja[] võisltejad = new Võistleja[2];
+
+    //vaja määrata alustaja
+    //Võistleja alustaja = võistlejad[Math.round(Math.random())];
+
+    public int kasutajaRünnak() {
+
+    }
+
+    public int kasutajaKaitse() {
+
+    }
+
+    public int arvutiRünnak() {
+
+    }
+
+    public int arvutiKaitse() {
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
 }
