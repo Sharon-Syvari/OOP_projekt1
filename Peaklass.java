9c4eea45682a344954fd4323bdfc3a78fd1432f7
Sharon Süvari <92800043+Sharon-Syvari@users.noreply.github.com>
2022-04-06 15:20:16 +0300

    Update Peaklass.java

--- Peaklass.java
+++ Peaklass.java
@@ -1,24 +1,71 @@
-package src
+package src.Rühmatöö_1;
+
+import src.Võistleja;
 
 public class Peaklass {
+    // Siin vist luua erinevad vastased
+    //Võistleja võistleja1
+    //Võistleja võistleja2
+    //Võistleja võistleja3
+
+
 
     public static void main(String[] args) {
         //Mingi jutt alustuseks
 
-        //Esitletakse reeglid
+        //Esitletakse reeglid:
+        // 1. Valid enda tegelasele nime
+        // 2. Valid endale vastase
+        // 3. Vastase tugevuse põhjal loositakse Sulle uudne IT-teemaline projektiidee,
+        // mis võib hakata sisse tooma miljoneid või hoopis mõned kopikad.
+        // Rahasumma kujutab endast punktide hulka, mis Sa võidad, kui oma rühmakaaslasele pähe teed.
+        // 4. Loositakse esimene ründaja
+        // 5. Igal käigul üks võislteja ründab ja teine kaitseb.
+        // Kasutaja saab valida kummalgi korral kas ühe terve käigu või
+        // kaks osalist käiku. Käikudel on kindlad tugevused, mis määrab punktide vahemiku,
+        // mille hulgast loositakse käigi lõplik punktide hulk. Näiteks kui valida käik 1,
+        // mille tugevus on 10, siis sellest vahemikust loositakse punktid.
+        // Valikud antakse ette käigu ajal ning valiku tegemiseks tuleb sisestada käigule vastav number.
+        // 6. Mõlema rühmaliikme saadud punktid avaldatakse korraga ning 1 elu kaotab see,
+        // kelle punktide hulk on väiksem. Võrdse seisu korral ei kaota kumbki.
+        // 7. Kaotab see, kes esimesena kõikidest eludest ilma jääb.
 
         //Küsitakse kasutaja nime
+        //  Kaklus.võistlejad[0] = Võistleja(...)
         //Esitletakse kasutaja omadused
 
         //Lastakse valida vastane(int)
+        //  Kaklus.võistlejad[1] = Võistleja(...)
 
         //Mäng algab.
         //Loositakse alustaja ja antakse tulemusest ekraanil teada
+        //  Võistleja alustaja = võistlejad[Math.round(Math.random())];
+
+
 
         //Igal korral lastakse valida kasutajal rünnaku - või kaitsevõte
         // ja selle põhjal loositakse tema punktid
 
+        //Kui alustab kasutaja
+        if alustaja = võistleja[0] {
+            while (võislteja[0].elud > 0 && võislteja[0].elud > 0)
+
+        }
+
+        //Kui alustab arvuti
+        if alustaja = võistleja[1] {
+            while (võislteja[0].elud > 0 && võislteja[0].elud > 0) {
+
+            }
+        }
+
         //Kaotab 1 elu see, kellel vähem punkte
+
+        // Kui arvuti kaotab
+        if (võislteja[0] == 0) {
+
+        } else { //kui kasutaja kaotab
+
+        }
     }
 }
-
