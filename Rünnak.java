c0ddccccffb3507635a27ac878e47fc8af31bc9e
susmet <82086976+susmet@users.noreply.github.com>
2022-04-07 12:15:22 +0300

    Update and rename Rünnakud.java to Rünnak.java

---
+++ Rünnak.java
@@ -0,0 +1,23 @@
+public class Rünnak {
+    private String nimetus;
+    private int tugevus;
+
+    public Rünnak(String nimetus, int tugevus) {
+        this.nimetus = nimetus;
+        this.tugevus = tugevus;
+    }
+
+    public int getTugevus() {
+        return tugevus;
+    }
+
+    public String getNimetus() {
+        return nimetus;
+    }
+
+
+    @Override
+    public String toString() {
+        return nimetus + " (Tugevus: "+ tugevus + ")";
+    }
+}
