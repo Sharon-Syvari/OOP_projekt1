ed0e6cc9b1a623ca831dfadf363d1f23c38ff784
susmet <82086976+susmet@users.noreply.github.com>
2022-04-07 12:13:59 +0300

    Update and rename Kaitsed.java to Kaitse.java

---
+++ Kaitse.java
@@ -0,0 +1,22 @@
+public class Kaitse {
+    private String nimetus;
+    private int tugevus;
+
+    public Kaitse(String nimetus, int tugevus) {
+        this.nimetus = nimetus;
+        this.tugevus = tugevus;
+    }
+
+    public String getNimetus() {
+        return nimetus;
+    }
+
+    public int getTugevus() {
+        return tugevus;
+    }
+
+    @Override
+    public String toString() {
+        return nimetus + " (Tugevus: "+ tugevus + ")";
+    }
+}
