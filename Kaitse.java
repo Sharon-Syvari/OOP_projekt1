edbee63d940beb3d03893802263a13fda87748a0
Sharon Süvari <92800043+Sharon-Syvari@users.noreply.github.com>
2022-04-07 17:08:13 +0300

    Update and rename Kaitsed.java to Kaitse.java

---
+++ Kaitse.java
@@ -0,0 +1,25 @@
+
+
+public class Kaitse {
+    private String nimetus;
+    private int tugevus;
+
+    public Kaitse(String nimetus, int tugevus) {
+        this.nimetus = nimetus;
+        this.tugevus = tugevus;
+    }
+
+    public String getNimetus() {
+        return nimetus;
+    }
+
+    public int getTugevus() {
+        return tugevus;
+    }
+
+    @Override
+    public String toString() {
+        return nimetus + " (Tugevus: "+ tugevus + ")";
+    }
+}
+
